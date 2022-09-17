7f03dd4d1c1508511b70b218ae5556f32ddcb18b
Dmitriy2511 <113593837+Dmitriy2511@users.noreply.github.com>
2022-09-17 10:47:22 +0300

    Add files via upload

---
+++ Java_5/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        new Timer(1);
+
+        new Waiter(5, "message1!");
+
+        new Waiter(7, "message2!");
+
+    }
+
+}
